632f4247c8c2346840bce0a74823838136824151
elpif13 <113675207+elpif13@users.noreply.github.com>
2022-10-15 22:05:31 +0300

    Create DatabaseLogger.java

---
+++ DatabaseLogger.java
@@ -0,0 +1,12 @@
+package oopWithKodlamaioApp.core.logging;
+
+public class DatabaseLogger implements Logger{
+	
+	public void log(String data) {
+		
+		System.out.println("Data gönderildi: " + data);
+	}
+	
+	
+
+}
